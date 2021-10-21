20bc3d0b271dc0da68c7829cfbf75204435236e6
Daman Behl <damanbehl@gmail.com>
2021-10-21 18:19:30 -0400

    detail page

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,4 +1,14 @@
-const MeetupId = () => {
-  return <h1>meetup details</h1>;
-};
-export default MeetupId;
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      address="Agra"
+      title="Agra visit"
+      description="our first meetup"
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Taj_Mahal_in_India_-_Kristian_Bertel.jpg/1280px-Taj_Mahal_in_India_-_Kristian_Bertel.jpg"
+    />
+  );
+}
+
+export default MeetupDetails;
