e9aa99a8ce7c900f7617a78fa9f0c968a494ddf1
Daman <damanbehl@gmail.com>
2021-11-07 15:20:34 -0500

    connection string  bug fixed

--- utility/mongo-conn.js
+++ utility/mongo-conn.js
@@ -3,7 +3,8 @@ import { MongoClient, ObjectId } from "mongodb";
 const getClient = async () => {
   let client = {};
   try {
-    client = await MongoClient.connect(process.env.DB_CONN_STRING);
+    const DB_CONN_STRING = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+    client = await MongoClient.connect(DB_CONN_STRING);
     return { client: client, error: null };
   } catch (error) {
     console.log("error while gettind db object mongoconn");
